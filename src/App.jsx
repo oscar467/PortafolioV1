2ddd5a43f1719ec150173b75785a52425b86e71e
oscar bejarano <osc4r.yt@gmail.com>
2023-04-13 10:51:25 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <div>
+        <a
+          href="https://www.linkedin.com/in/oscar-bejarano-ab1568241/"
+          target="_blank"
+        >
+          <img
+            src="https://res.cloudinary.com/dpnnlqglr/image/upload/v1681192052/OBlogoNofondoGrande_npoe5q.png"
+            className="logo"
+            alt="Oscar logo"
+          />
+        </a>
+      </div>
+      <h1>Portfolio under construction...</h1>
+      <div className="card">
+        <button
+          onClick={() =>
+            (location.href =
+              "https://www.linkedin.com/in/oscar-bejarano-ab1568241/")
+          }
+        >
+          Contac me
+        </button>
+      </div>
+      <p>Created by Oscar Bejarano</p>
+    </div>
+  );
+}
+
+export default App;
